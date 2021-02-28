641b79f4ad90845ecd7c0b52f727c414f1cf2b5f
Wizard Hat <Nlarsen23.student@gmail.com>
2021-02-28 22:26:54 +0000

    Switched to django

--- backend/message.py
+++
@@ -1,7 +0,0 @@
-class Message:
-    def __init__(self, author, contents):
-        self.author = author
-        self.contents = contents
-
-    def json(self):
-        return {"author-id": self.author, "contents": self.contents}
--- backend/user.py
+++
@@ -1,3 +0,0 @@
-class User:
-    def __init__(self, user_id):
-        self.id = user_id
